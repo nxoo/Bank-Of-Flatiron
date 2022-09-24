8d4e1a43fe6a97f4117f7684c1517e7f78b49629
nxoo <nxoo.dev@gmail.com>
2022-09-24 18:50:34 +0300

    listing transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function Transaction() {
-  return (
-    <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-    </tr>
-  );
+    const [transactions, setTransactions] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:8001/transactions')
+            .then((res) => res.json())
+            .then((data) => setTransactions(data));
+    }, [])
+    console.log('tt', transactions)
+    if (transactions.length === 0) return null
+    return (
+        transactions.map(transaction => {
+            return (
+                <tr key={transaction.id}>
+                    <td>
+                        <h3 className="ui center aligned header">{transaction.date}</h3>
+                    </td>
+                    <td>
+                        <h3 className="ui center aligned header">{transaction.description}</h3>
+                    </td>
+                    <td>
+                        <h3 className="ui center aligned header">{transaction.category}</h3>
+                    </td>
+                    <td>
+                        <h3 className="ui center aligned header">{transaction.amount}</h3>
+                    </td>
+                </tr>
+            )
+        })
+    );
 }
 
 export default Transaction;
